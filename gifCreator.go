a26034fa8b4a98d3688c1018554b450455ef5597
GaryB <165720413+gary23b@users.noreply.github.com>
2024-05-30 14:25:08 -0600

    Added comments. added MaxSpeed. added IsPenDown().

--- gifCreator.go
+++ gifCreator.go
@@ -9,11 +9,14 @@ import (
 	"github.com/gary23b/easygif"
 )
 
+// Take an image of the turtle program window's contents and save it as a PNG.
 func TakeScreenshot(window Window, outputPNGPath string) error {
 	screenshot := window.GetCanvas().GetScreenshot()
 	return easygif.SaveImageToPNG(screenshot, outputPNGPath)
 }
 
+// Take a set of screenshots of the turtle program window's content and return a slice of images.
+// The length of the captured video will be delayBetweenScreenshots * frameCount
 func TakeScreenshotVideo(
 	window Window,
 	delayBetweenScreenshots time.Duration,
@@ -36,6 +39,8 @@ func TakeScreenshotVideo(
 }
 
 // Start this as a go routine to create a GIF of your creation.
+// The length of the captured video will be delayBetweenScreenshots * frameCount
+// The length of the gif will be delayBetweenGifFrames * frameCount
 func CreateGif(
 	window Window,
 	delayBetweenScreenshots time.Duration,
@@ -57,6 +62,9 @@ func CreateGif(
 }
 
 // Start this as a go routine to create a GIF of your creation.
+// Dithering will be used to achieve more colors, but images artifacts are introduced. This is best for images with lots of shading.
+// The length of the captured video will be delayBetweenScreenshots * frameCount
+// The length of the gif will be delayBetweenGifFrames * frameCount
 func CreateGifDithered(
 	window Window,
 	delayBetweenScreenshots time.Duration,
